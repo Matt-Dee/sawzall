165d21afe71bd89c0eef277911e938be619d4376
Matt Donnelly <mdonnelly@new-host.home>
2013-12-02 21:54:29 -0500

    created basic index and query messages for the actor system.  these are just basic.  I'm sure it will get more complicated.

---
+++ sawzall-message/src/main/java/org/sawzall/message/SearchRequest.java
@@ -0,0 +1,24 @@
+package org.sawzall.message;
+
+import org.joda.time.DateTime;
+
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mdonnelly
+ * Date: 12/2/13
+ * Time: 8:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SearchRequest{
+    Map<String, String> searchFieldsAndTerms;
+    DateTime date;
+
+    public Map<String, String> getSearchFieldsAndTerms() {return searchFieldsAndTerms;}
+    public void setSearchFieldsAndTerms(Map<String, String> searchFieldsAndTerms) {this.searchFieldsAndTerms = searchFieldsAndTerms;}
+    public void addSearchFieldAndTerm(String field, String term){ searchFieldsAndTerms.put(field, term); }
+
+    public DateTime getDate() {return date;}
+    public void setDate(DateTime date) {this.date = date;}
+}
